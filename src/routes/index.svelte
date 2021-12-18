0fdb0eaa0a25842a890f7fec1382b18acd8e545f
Steven Hildreth <sphildreth@gmail.com>
2021-12-17 21:54:28 -0600

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p class="m-3">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="m-10">
+    <button class="btn btln-lg">Da Button</button>
+</div>
