b0429524d936aaa8d61c72c963f5c9bc1a6dabe8
Steven Hildreth <sphildreth@gmail.com>
2021-12-28 15:40:21 -0600

    checkpoint commit

--- src/lib/NavBarLinks.svelte
+++ src/lib/NavBarLinks.svelte
@@ -1,34 +1,13 @@
 <script>
   import { page, session } from '$app/stores';
-  const showAdminMenu =
-    $session.user && ($session.user.isEditor || $session.user.isManager || $session.user.isEditor);
+  const showAdminMenu = $session.user && ($session.user.isAdmin || $session.user.isManager || $session.user.isEditor);
 </script>
 
-<a
-  class="btn btn-ghost btn-sm rounded-btn"
-  class:active={$page.path === '/publishers'}
-  href="/publishers">Publishers</a
->
-<a
-  class="btn btn-ghost btn-sm rounded-btn"
-  class:active={$page.path === '/franchises'}
-  href="/franchises">Franchises</a
->
-<a
-  class="btn btn-ghost btn-sm rounded-btn"
-  class:active={$page.path === '/creators'}
-  href="/creators">Creators</a
->
-<a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path === '/series'} href="/series"
-  >Series</a
->
-<a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path === '/issues'} href="/issues"
-  >Issues</a
->
+<a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path === '/publishers'} href="/publishers">Publishers</a>
+<a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path === '/franchises'} href="/franchises">Franchises</a>
+<a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path === '/creators'} href="/creators">Creators</a>
+<a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path === '/series'} href="/series">Series</a>
+<a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path === '/issues'} href="/issues">Issues</a>
 {#if $session.user && showAdminMenu}
-  <a
-    class="btn btn-ghost btn-sm rounded-btn"
-    class:active={$page.path.indexOf('/admin') > -1}
-    href="/admin">Admin</a
-  >
+  <a class="btn btn-ghost btn-sm rounded-btn" class:active={$page.path.indexOf('/admin') > -1} href="/admin">Admin</a>
 {/if}
