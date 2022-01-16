aa084a452e2ffeddd64e620bb747dde439f68461
Steven Hildreth <sphildreth@gmail.com>
2022-01-15 18:30:40 -0600

    checkpoint commit

---
+++ src/lib/components/PublisherMiniCard.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let publisher;
+
+  function gotoPublisher() {
+    window.location = `/publisher/${publisher.id}`;
+  }
+</script>
+
+<div class="card is-clickable" data-publisher-id={publisher.id} on:click={gotoPublisher}>
+  <div class="card-image" />
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-32x32">
+          <img src={publisher.imageUrl} alt={publisher.shortName} />
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-6 mb-2">{publisher.shortName}</p>
+        <div class="title-data-container">
+          <div>
+            <span class="is-size-7">Series {publisher.seriesCount || 0}</span>
+          </div>
+          <div>
+            <span class="is-size-7">Franchises {publisher.franchisesCount || 0}</span>
+          </div>
+          <div>
+            <span class="is-size-7">Issues: {publisher.issuesCount || 0}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .title-data-container {
+    font-size: smaller;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,33 @@
 <script>
   import { session } from '$app/stores';
+  import * as api from '$lib/api.js';
+  import { onMount } from 'svelte';
+  import { toast } from 'bulma-toast';
+  import PublisherMiniCard from '$lib/components/PublisherMiniCard.svelte';
+
+  let publisherData = [];
+
+  onMount(async () => {
+    await getPublisherData();
+  });
+
+  async function getPublisherData() {
+    const response = await api.post(
+      `publishers`,
+      {
+        pageSize: 3,
+        page: 1,
+        orderBy: 'issueCount',
+        orderDir: 'desc',
+      },
+      $session.user.token,
+    );
+    if (response.isSuccess) {
+      publisherData = response.data;
+      return;
+    }
+    toast({ message: 'Unable to get Publishers!', position: 'top-center', type: 'is-error' });
+  }
 </script>
 
 <svelte:head>
@@ -33,7 +61,11 @@
       <div class="message-header">
         <p><span class="icon"><i class="fas fa-building" /></span> Publishers</p>
       </div>
-      <div class="message-body">Put top 3 publishers here.</div>
+      <div class="message-body">
+        {#each publisherData as publisher}
+          <PublisherMiniCard {publisher} />
+        {/each}
+      </div>
     </article>
   </div>
   <div class="column is-one-quarter">
@@ -53,7 +85,3 @@
     </article>
   </div>
 </div>
-
-{#if $session.user}
-  User is logged in.
-{/if}
